3c2c78a13f33bbedeb22f979c6ed4379cc94e3c4
knowledge <knowledgerelated96@gmail.com>
2020-07-04 14:23:22 +0530

    test from knowledge

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use('/api/userModel', require('./Api/User'));
 //app.use(cors()); 
 //commit form githubdesktop
 //commit form cli to git repo (from project path directly commmit)
+//commit from knowledge
 const Port = process.env.PORT || 5000;   
 
 app.listen(Port, () => console.log('Server started'));
