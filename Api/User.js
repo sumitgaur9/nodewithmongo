d48eb23ab24ef87fc25ff7b569d870968f504d94
SumitGaur92 <65018423+SumitGaur92@users.noreply.github.com>
2020-07-07 22:38:23 +0530

    /users/me/logout and /users/me/logoutall

--- Api/User.js
+++ Api/User.js
@@ -41,6 +41,29 @@ route.get('/users/me', auth, async(req, res) => {
   res.send(req.user)
 })
 
+route.post('/users/me/logout', auth, async (req, res) => {
+    // Log user out of the application
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+route.post('/users/me/logoutall', auth, async(req, res) => {
+    // Log user out of all devices
+    try {
+        req.user.tokens.splice(0, req.user.tokens.length)
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
 
 
 
