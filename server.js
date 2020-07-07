26ef4e91606c65e64bfcf98d60fa119045dff2fb
knowledge <knowledgerelated96@gmail.com>
2020-07-07 09:30:45 +0530

    Update server.js
    add cors

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const express = require('express');
 const connectDB = require('./DB/Conncection');
 const app = express();
-
+var cors = require('cors');
 
 connectDB();
 app.use(express.json({ extended: false }));
 app.use('/api/userModel', require('./Api/User'));
-//app.use(cors()); 
+app.use(cors()); 
 //commit form githubdesktop
 //commit form cli to git repo (from project path directly commmit)
 //commit from knowledge
