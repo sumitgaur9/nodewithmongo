3a70263ec3a61c54b24642ed074393d08d6a8bf9
QTPL0023 <sumit.gaur@qtsolv.com>
2020-08-30 11:02:56 +0530

    process.env.YOUR_API_KEY added

--- Api/User.js
+++ Api/User.js
@@ -44,8 +44,8 @@ const fast2sms = require('fast-two-sms')
 route.post('/GenerateOTP', async (req, res) => {
   try {
      let otp = Math.floor(Math.random()*1000000);
-     var options = { authorization: 'DHfOUwAJ107WP2YN5pqhRo3zcKlITjXaM9tGrFQx8mv4i6nZydsW15y4bSw2qHGoBQEYpjIakKTgnUVu', message: 'Your HealthCare App account OTP to change password is: '+ otp, numbers: [ req.body.TARGET_PHONE_NUMBER] }
-    //var options = { authorization: process.env.YOUR_API_KEY, message: 'Your HealthCare App account OTP to change password is: '+ otp, numbers: [ req.body.TARGET_PHONE_NUMBER] }
+     //var options = { authorization: 'DHfOUwAJ107WP2YN5pqhRo3zcKlITjXaM9tGrFQx8mv4i6nZydsW15y4bSw2qHGoBQEYpjIakKTgnUVu', message: 'Your HealthCare App account OTP to change password is: '+ otp, numbers: [ req.body.TARGET_PHONE_NUMBER] }
+    var options = { authorization: process.env.YOUR_API_KEY, message: 'Your HealthCare App account OTP to change password is: '+ otp, numbers: [ req.body.TARGET_PHONE_NUMBER] }
     const response = await fast2sms.sendMessage(options)
     response.OTP = otp;
     console.log(response)
